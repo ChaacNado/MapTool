08df893c883e7ebb027bd3576c9228337f963c6b
patrik-nilsson <patriknilson@hotmail.com>
2019-10-21 15:59:58 +0200

    Rooms now exist
    The tool now actually creates individual rooms which we could access.

--- MapTool/Assets/BoardManager.cs
+++ MapTool/Assets/BoardManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoardManager : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class BoardManager : MonoBehaviour
     public int boardColumns;
 
     public GameObject[,] tiles;
-    public List<Tuple<int, int>> idk;
+    public List<List<Tuple<int, int>>> idk;
 
     public GameObject tilePrefabIGuess;
 
@@ -17,7 +18,7 @@ public class BoardManager : MonoBehaviour
     void Awake()
     {
         tiles = new GameObject[boardRows, boardColumns];
-        idk = new List<Tuple<int, int>>();
+        idk = new List<List<Tuple<int, int>>>();
         for (int i = 0; i < boardRows; i++)
         {
             for (int j = 0; j < boardColumns; j++)
@@ -50,39 +51,45 @@ public class BoardManager : MonoBehaviour
 
     public void GenerateSpaces()
     {
-        int space = 0;
-        for (int i = 0; i < tiles.GetLength(0); i++)
+        idk.Clear();
+        int currentList = 0;
+        for (int x = 0; x < boardRows; x++)
         {
-            for (int j = 0; j < tiles.GetLength(1); j++)
+            for (int y = 0; y < boardColumns; y++)
             {
-                if (tiles[i, j].tag != "Road")
+                if (x < boardRows && y < boardColumns && tiles[x, y].tag != "Road" && tiles[x, y].tag != "Occupado")
                 {
-                    int x = j;
-                    while (tiles[i, j].tag != "Road")
+                    int currentX = x;
+                    idk.Add(new List<Tuple<int, int>>());
+                    while (y < boardColumns && tiles[x, y].tag != "Road" && tiles[x, y].tag != "Occupado")
                     {
-                        if (tiles[i, j].tag == "Road")
+                        idk[currentList].Add(new Tuple<int, int>(x, y));
+                        tiles[x, y].tag = "Occupado";
+                        x++;
+                        while (x < boardRows && tiles[x, y].tag != "Road" && tiles[x, y].tag != "Occupado")
                         {
-                            j = x;
-                            i++;
+                            idk[currentList].Add(new Tuple<int, int>(x, y));
+                            tiles[x, y].tag = "Occupado";
+                            x++;
                         }
-                        else
-                        {
-                            idk.Add(new Tuple<int, int>(i, j));
-                            j++;
-                        }
-                        if (i >= tiles.GetLength(0) || j >= tiles.GetLength(1))
-                        {
-                            break;
-                        }
+                        x = currentX;
+                        y++;
                     }
-                    j = x;
-                    space++;
+                    currentList++;
                 }
             }
         }
-        foreach (Tuple<int, int> FUCK in idk)
+
+        foreach (List<Tuple<int, int>> shit in idk)
         {
-            tiles[FUCK.Item1, FUCK.Item2].GetComponent<SpriteRenderer>().color = Color.blue;
+            float r = UnityEngine.Random.Range(0f, 1f);
+            float g = UnityEngine.Random.Range(0f, 1f);
+            float b = UnityEngine.Random.Range(0f, 1f);
+            foreach (Tuple<int, int> FUCK in shit)
+            {
+
+                tiles[FUCK.Item1, FUCK.Item2].GetComponent<SpriteRenderer>().color = new Color(r, g, b);
+            }
         }
     }
 
